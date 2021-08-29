e53bf0f5ccb1771a52fa005bed638483b1c426dc
fuzhongkai@gmail.com <fuzhongkai@gmail.com>
2021-08-29 20:12:13 +0000

    Add Seq2SeqClassification to Web APIs project

---
+++ SeqWebAPIs/Controllers/Seq2SeqClassificationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvUtils;
+using SeqWebAPIs;
+
+namespace Seq2SeqClassificationWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Seq2SeqClassificationController : ControllerBase
+    {
+
+        private readonly ILogger<Seq2SeqClassificationController> _logger;
+
+        public Seq2SeqClassificationController(ILogger<Seq2SeqClassificationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{key}/{input}")]
+        public string Get(string key, string input)
+        {
+            List<string> inputGroups = input.Split('\t').ToList();
+            (var tag, var text) = Seq2SeqClassificationInstances.Call(key, inputGroups);
+
+            Logger.WriteLine($"'{key}' | '{input}' -> '{tag}' | '{text}'");
+            return tag + "\t" + text;
+        }
+    }
+}
---
+++ SeqWebAPIs/Seq2SeqClassificationInstances.cs
@@ -0,0 +1,58 @@
+﻿using AdvUtils;
+using Seq2SeqSharp.Applications;
+using Seq2SeqSharp.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeqWebAPIs
+{
+    static public class Seq2SeqClassificationInstances
+    {
+        private static object locker = new object();
+        private static Dictionary<string, Seq2SeqClassification> m_key2Instance = new Dictionary<string, Seq2SeqClassification>();
+
+        static public void Initialization(Dictionary<string, string> key2ModelFilePath, int maxTestSrcSentLength, int maxTestTgtSentLength, string processorType, string deviceIds)
+        {
+            foreach (var pair in key2ModelFilePath)
+            {
+                Logger.WriteLine($"Loading '{pair.Key}' model from '{pair.Value}'");
+                Seq2SeqClassificationOptions opts = new Seq2SeqClassificationOptions();
+                opts.ModelFilePath = pair.Value;
+                opts.MaxTestSrcSentLength = maxTestSrcSentLength;
+                opts.MaxTestTgtSentLength = maxTestTgtSentLength;
+                opts.ProcessorType = processorType;
+                opts.DeviceIds = deviceIds;
+
+                var inst = new Seq2SeqClassification(opts);
+
+                m_key2Instance.Add(pair.Key, inst);
+            }
+        }
+        static public (string, string) Call(string key, List<string> inputFeatureGroups)
+        {
+
+            List<List<List<string>>> groupBatchTokens = new List<List<List<string>>>();
+            foreach (var inputFeatureGroup in inputFeatureGroups)
+            {
+                List<string> tokens = inputFeatureGroup.Split(' ').ToList();
+                List<List<string>> batchTokens = new List<List<string>>();
+                batchTokens.Add(tokens);
+                groupBatchTokens.Add(batchTokens);
+            }
+
+            lock (locker)
+            {
+                List<NetworkResult> nrs = m_key2Instance[key].Test(groupBatchTokens);
+                var nrCLS = nrs[0];
+                var nrSeq2Seq = nrs[1];
+
+                string tag = nrCLS.Output[0][0][0];
+                string text = String.Join(" ", nrSeq2Seq.Output[0][0].ToArray(), 1, nrSeq2Seq.Output[0][0].Count - 2);
+
+                return (tag, text);
+            }
+        }
+    }
+}
