04869001b7c7bae08ab3d96b926fb5769d1c5f7a
fuzhongkai@gmail.com <fuzhongkai@gmail.com>
2021-08-31 19:15:19 +0000

    Load models for Seq2SeqWebAPIs from command line

--- SeqWebAPIs/Program.cs
+++ SeqWebAPIs/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Seq2SeqSharp.Utils;
+using SeqClassificationWebAPI;
+using SeqSimilarityWebAPI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,32 @@ namespace SeqWebAPIs
         public static void Main(string[] args)
         {
             Logger.LogFile = $"{nameof(SeqWebAPIs)}_{Utils.GetTimeStamp(DateTime.Now)}.log";
+
+            if (args.Length != 6)
+            {
+                Logger.WriteLine(Logger.Level.err, $"SeqWebAPIs.exe [tsc model file path] [sentSim model file path] [hpi model file path] [exam model file path] [assessment model file path] [results model file path]");
+                return;
+            }
+
+            string tscModelFilePath = args[0];
+            string sentSimModelFilePath = args[1];
+            string hpiModelFilePath = args[2];
+            string examModelFilePath = args[3];
+            string assessmentModelFilePath = args[4];
+            string resModelFilePath = args[5];
+
+
+            SeqClassificationInstance.Initialization(tscModelFilePath, 512, "CPU", "0");
+            SeqSimilarityInstance.Initialization(sentSimModelFilePath, 512, "CPU", "0");
+
+            Dictionary<string, string> key2ModelFilePath = new Dictionary<string, string>();
+            key2ModelFilePath.Add("hpi", hpiModelFilePath);
+            key2ModelFilePath.Add("pe", examModelFilePath);
+            key2ModelFilePath.Add("ap", assessmentModelFilePath);
+            key2ModelFilePath.Add("res", resModelFilePath);
+
+            Seq2SeqClassificationInstances.Initialization(key2ModelFilePath, 1024, 256, "CPU", "0");
+
 
             CreateHostBuilder(args).Build().Run();
         }
