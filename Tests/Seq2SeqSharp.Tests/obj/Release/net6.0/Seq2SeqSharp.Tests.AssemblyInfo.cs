fef612222c266af935b59528643f42ba5a9d7017
Zhongkai Fu <fuzhongkai@gmail.com>
2022-07-28 15:17:34 -0700

    Delete binary files

--- Tests/Seq2SeqSharp.Tests/obj/Release/net6.0/Seq2SeqSharp.Tests.AssemblyInfo.cs
+++ Tests/Seq2SeqSharp.Tests/obj/Release/net6.0/Seq2SeqSharp.Tests.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seq2SeqSharp.Tests")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seq2SeqSharp.Tests")]
