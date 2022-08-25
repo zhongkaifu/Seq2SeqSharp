dd11557581c6e9b69f1a6f8597013c3ef88afdf7
Zhongkai Fu <fuzhongkai@gmail.com>
2022-08-24 17:43:28 -0700

    Add file headers

--- Seq2SeqSharp/Models/Seq2SeqModel.cs
+++ Seq2SeqSharp/Models/Seq2SeqModel.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿// Copyright (c) Zhongkai Fu. All rights reserved.
+// https://github.com/zhongkaifu/Seq2SeqSharp
+//
+// This file is part of Seq2SeqSharp.
+//
+// Seq2SeqSharp is licensed under the BSD-3-Clause license found in the LICENSE file in the root directory of this source tree.
+//
+// Seq2SeqSharp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD-3-Clause License for more details.
+
+using System;
 using System.Linq;
 using Seq2SeqSharp.Models;
 using Seq2SeqSharp.Utils;
