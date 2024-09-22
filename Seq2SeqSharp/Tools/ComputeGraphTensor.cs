90e9f8e783564dc3bd6a519ef36ca226d8704f86
Zhongkai Fu <fuzhongkai@gmail.com>
2024-09-22 09:45:17 -0700

    Revert "minor update"
    This reverts commit b004567e24e09f902057f822c106c605e28e4f85.

--- Seq2SeqSharp/Tools/ComputeGraphTensor.cs
+++ Seq2SeqSharp/Tools/ComputeGraphTensor.cs
@@ -4054,7 +4054,7 @@ namespace Seq2SeqSharp.Tools
 
 
             var lossTrue = Gather(loss, scatterIdxTensor, 1, runGradients: false);
-            var lossValue = lossTrue.ToWeightArray().Sum() / loss.ElementCount;
+            var lossValue = lossTrue.ToWeightArray().Sum();
 
             return (lossValue, loss);
         }
